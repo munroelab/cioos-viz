526eabf644018857cf6a945a55f1d00e0b768d92
RineZaman <rzaman@mun.ca>
2021-10-05 09:37:07 -0230

    Added Multistation temprature plot of CMAR Data

--- W2/CMAR_staticplot.py
+++ W2/CMAR_staticplot.py
@@ -3,18 +3,20 @@ from bokeh.plotting import figure
 from bokeh.models import LinearAxis, Range1d, DatetimeTickFormatter
 import pandas as pd
 import numpy as np
+from erddapy import ERDDAP
 
-df = pd.read_csv("C:/Users/rinez/Downloads/wpsu-7fer_raw_merged.csv")
+e = ERDDAP(server="https://dev.cioosatlantic.ca/erddap",
+                 protocol="tabledap", )
+e.auth = ("cioosatlantic", "4oceans")
+e.response = "csv"
+e.dataset_id = 'wpsu-7fer'
+df = e.to_pandas()
 df = df.dropna(axis=1, how='all')
-df = df.sort_values(by=['timestamp'])
-#df = df.set_index(df.timestamp.astype(np.datetime64))
-data = df[df.station == "Long Beach"]
-data = data[data.depth == 5]
-time = pd.to_datetime(data['timestamp'], format="%Y-%m-%dT%H:%M:%S.000")
+df = df.sort_values(by=['time (UTC)'])
+data = df[df.waterbody_station == "St. Mary's Bay-Long Beach"]
+data = data[data['depth (m)'] == 5]
+time = pd.to_datetime(data['time (UTC)'], format="%Y-%m-%dT%H:%M:%SZ")
 
-
-# oxy = data.dropna(axis=0 ,how='any', subset=['Dissolved Oxygen'])
-# temp = data.dropna(axis=0, how='any', subset = ['Temperature'])
 p = figure(plot_width= 1000, plot_height=400, x_axis_type="datetime",
            title = "Long Beach - Digby County",)
 p.xaxis.formatter=DatetimeTickFormatter(
@@ -23,12 +25,14 @@ p.xaxis.formatter=DatetimeTickFormatter(
         months=["%d %B %Y"],
         years=["%d %B %Y"],)
 p.yaxis.axis_label = "Dissolved Oxygen (% saturation)"
-p.extra_y_ranges = {"foo":Range1d(start=data.Temperature.min(), end=data.Temperature.max()),
-                    "who":Range1d(start=data['Dissolved Oxygen'].min(), end = data['Dissolved Oxygen'].max())}
-p.add_layout(LinearAxis(y_range_name="foo", axis_label="Temperature (Celsius)"), "right")
-p.line(time, data['Dissolved Oxygen'], line_width=2,
+p.extra_y_ranges = {"foo":Range1d(start=data['Temperature (degrees Celsius)'].min(),
+                                  end=data['Temperature (degrees Celsius)'].max()),
+                    "who":Range1d(start=data['Dissolved_Oxygen (% saturation)'].min(),
+                                  end = data['Dissolved_Oxygen (% saturation)'].max())}
+p.add_layout(LinearAxis(y_range_name="foo", axis_label="Temperature (°C)"), "right")
+p.line(time, data['Dissolved_Oxygen (% saturation)'], line_width=2,
         legend_label='Dissolved Oxygen at 5m Depth', y_range_name="who")
-p.line(time, data['Temperature'], line_width=2
+p.line(time, data['Temperature (degrees Celsius)'], line_width=2
        ,line_color="green", y_range_name="foo", legend_label="Temperature at 5m Depth")
 p.legend.location= 'top_right'
 p.legend.click_policy='hide'
