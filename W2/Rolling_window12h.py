7414a3439ad0d5502dc2bee73e8251bb88d1abd0
RineZaman <rzaman@mun.ca>
2021-10-06 08:01:35 -0230

    Added Multistation temprature plot of CMAR Data

---
+++ W2/Rolling_window12h.py
@@ -0,0 +1,47 @@
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.models import LinearAxis, Range1d, DatetimeTickFormatter,  Legend
+import datetime as dt
+import pandas as pd
+import numpy as np
+from erddapy import ERDDAP
+import scipy.signal
+import os
+#import matplotlib.pyplot as plt
+
+print("dataset loading")
+if os.path.exists("dataset.csv"):
+    df = pd.read_csv("dataset.csv")
+else:
+    e = ERDDAP(server="https://dev.cioosatlantic.ca/erddap",
+                 protocol="tabledap", )
+    e.auth = ("cioosatlantic", "4oceans")
+    e.response = "csv"
+    e.dataset_id = 'wpsu-7fer'
+    df = e.to_pandas()
+    df.to_csv("dataset.csv")
+
+
+print("dataset loaded, filtering...")
+df = df.dropna(axis=1, how='all')
+df = df.sort_values(by=['time (UTC)'])
+df['time (UTC)'] = pd.to_datetime(df['time (UTC)'], format="%Y-%m-%dT%H:%M:%SZ")
+df = df.set_index(df['time (UTC)'].astype(np.datetime64))
+data = df[df.waterbody_station == "St. Mary's Bay-Long Beach"]
+data = data[data['depth (m)'] == 5]
+data = data.loc['2020-8-1':'2020-8-30']
+data = data[['Temperature (degrees Celsius)','Dissolved_Oxygen (% saturation)']]
+maxdata = data.resample('0.5D').max()
+mindata = data.resample('0.5D').min()
+print("dataset filtered and resampled")
+p= figure( plot_width=600, plot_height=400, output_backend="webgl")
+p.xaxis.formatter=DatetimeTickFormatter(
+        hours=["%H:%M"],
+        days=["%a %b %d"],
+        months=["%b %Y"],
+        years=["%Y"],)
+p.yaxis.axis_label = "Temperature Peaks per 12 hours (°C)"
+p.line(data.index, maxdata['Temperature (degrees Celsius)'], line_width = 2, color='red')
+p.line(data.index, mindata['Temperature (degrees Celsius)'],line_width = 2, color='blue')
+show(p)
+print("dataset resampled")
---
+++ W2/Spectra_example.py
@@ -0,0 +1,51 @@
+from bokeh.plotting import figure
+from bokeh.io import show
+#from bokeh.models import LinearAxis, Range1d, DatetimeTickFormatter,  Legend
+import datetime as dt
+import pandas as pd
+import numpy as np
+from erddapy import ERDDAP
+import scipy.signal
+import os
+#import matplotlib.pyplot as plt
+
+print("dataset loading")
+if os.path.exists("dataset.csv"):
+    df = pd.read_csv("dataset.csv")
+else:
+    e = ERDDAP(server="https://dev.cioosatlantic.ca/erddap",
+                 protocol="tabledap", )
+    e.auth = ("cioosatlantic", "4oceans")
+    e.response = "csv"
+    e.dataset_id = 'wpsu-7fer'
+    df = e.to_pandas()
+    df.to_csv("dataset.csv")
+
+
+print("dataset loaded, filtering...")
+df = df.dropna(axis=1, how='all')
+df = df.sort_values(by=['time (UTC)'])
+df['time (UTC)'] = pd.to_datetime(df['time (UTC)'], format="%Y-%m-%dT%H:%M:%SZ")
+df = df.set_index(df['time (UTC)'].astype(np.datetime64))
+data = df[df.waterbody_station == "St. Mary's Bay-Long Beach"]
+data = data[data['depth (m)'] == 5]
+data = data.loc['2020-8-1':'2020-8-2']
+data = data[['time (UTC)','Temperature (degrees Celsius)','Dissolved_Oxygen (% saturation)']]
+data['time (UTC)'] = (data["time (UTC)"]-dt.datetime(1970,1,1)).dt.total_seconds()
+data['time (UTC)'] -= data['time (UTC)'].min()
+data = data.rename(columns={'time (UTC)': "time (s)"})
+inter_sample_time = data['time (s)'][1]-data["time (s)"][0]
+sampling_frequency = 1/inter_sample_time
+print("# of samples =", len(data))
+print("sample time interval =", inter_sample_time, 's')
+print("sampling frequency =", sampling_frequency, 'hz')
+print("dataset filtered")
+
+f, psd= scipy.signal.periodogram(data['Temperature (degrees Celsius)'], fs=sampling_frequency)
+p = figure(plot_width= 1000, plot_height=400, title ='Power Spectral Density')
+p.line(f, psd, line_width=2)
+p.xaxis.axis_label = "Freq (Hz)"
+p.yaxis.axis_label = "PSD"
+show(p)
+
+
